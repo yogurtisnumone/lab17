dd681757f80dd9782abc3f9243f91057c73e56fd
yogurtisnumone <kachapat_p@cmu.ac.th>
2024-02-09 14:54:28 +0700

    17.3

--- lab17_3.cpp
+++ lab17_3.cpp
@@ -4,21 +4,36 @@
 #include<vector>
 using namespace std;
 
-vector<int> randomVector(int);
-void showVector(vector<int>);
-int dotProduct(vector<int>,vector<int>);
-
-int main(){
-	srand(time(0));
-	vector<int> x = randomVector(5);
-	vector<int> y = randomVector(5);
-	int z = dotProduct(x,y);
-	showVector(x);
-	cout << " . ";
-	showVector(y);
-	cout << " = " << z;
-	
-	return 0;
+vector<int> randomVector(unsigned int N){   
+    vector<int> v;
+    for(unsigned int i = 0; i < N; i++) v.push_back(rand()%10);
+    return v;
 }
 
-//Write Definition of randomVector() showVector() and dotProduct() here
+void showVector(vector<int> v){
+    cout << "[";
+    for(unsigned int i = 0; i < v.size(); i++){
+        cout << v[i];
+        if(i != v.size() - 1) cout << " ";
+        else cout << "]";
+    }
+}
+
+int dotProduct(vector<int> x, vector<int> y){
+    int sum = 0;
+    for(unsigned int i = 0; i < x.size(); i++) sum += x[i] * y[i];    
+    return sum;
+}
+
+int main(){
+ srand(time(0));
+ vector<int> x = randomVector(5);
+ vector<int> y = randomVector(5);
+ int z = dotProduct(x,y);
+ showVector(x);
+ cout << " . ";
+ showVector(y);
+ cout << " = " << z;
+ 
+ return 0;
+}
